4d7c0df282bffc2b4437f1e0fea055c99e4624fa
Tamagotchi9 <voloshko999@gmail.com>
2024-05-15 14:32:53 +0300

    remove import

--- app/articles/create/page.tsx
+++ app/articles/create/page.tsx
@@ -1,13 +1,11 @@
 import { auth } from "@/auth"
 
-import GoBackButton from "@/app/ui/elements/go-back-button";
 import {CreateForm} from "@/components/articles/create-form";
 export default async function Page() {
     const session = await auth();
 
     return (
         <main>
-            <GoBackButton route={'/articles'} />
             <CreateForm user={session?.user}/>
         </main>
     );
