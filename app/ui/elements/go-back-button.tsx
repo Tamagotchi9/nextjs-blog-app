04820070f6e35bb8c599ba510568348ffabf82be
Tamagotchi9 <voloshko999@gmail.com>
2025-07-09 21:34:25 +0300

    switched from chakra-ui to shadcn

--- app/ui/elements/go-back-button.tsx
+++ app/ui/elements/go-back-button.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import Link from "next/link";
-import {ArrowBackIcon} from "@chakra-ui/icons";
 
 export default function GoBackButton ({ route }: { route: string}) {
     return (
         <Link href={route}>
-            <ArrowBackIcon/> Back to articles
+            Back to articles
         </Link>
     )
 }
