1e2cbf472998077abc2da3d8595d39a3cc67a743
Tamagotchi9 <voloshko999@gmail.com>
2025-07-30 23:26:53 +0300

    added Article to definitions

---
+++ definitions/article.ts
@@ -0,0 +1,11 @@
+export interface Article {
+    id: string
+    authorId: string
+    title: string
+    content: string
+    description: string
+    imageUrl: string
+    createdAt: string
+    updatedAt: string
+    postedAt: string
+}
