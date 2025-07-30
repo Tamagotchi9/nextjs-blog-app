8d1b756e85d229fae309fe2db58a4c5492f86782
Tamagotchi9 <voloshko999@gmail.com>
2025-07-30 23:29:06 +0300

    changed create-form validation error text

--- schemas/article.ts
+++ schemas/article.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 
 export const ArticleFormSchema = z.object({
     title: z.string().min(3, {
-        message: 'title min 3'
+        message: 'at least 3 characters'
     }),
     content: z.string().min(5, {
-        message: 'content min 5'
+        message: 'at least 5 characters'
     }),
     imageUrl: z.string().optional()
 })
