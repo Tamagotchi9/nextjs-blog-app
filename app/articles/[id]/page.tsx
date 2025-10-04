0a49aa4eff5fb83b84ef4cf011838f92878b4818
Tamagotchi9 <voloshko999@gmail.com>
2025-10-04 16:35:02 +0300

    fix getting page params on ArticlePage

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -4,7 +4,7 @@ import {ArticleTable} from "@/drizzle/schema/article";
 import {eq} from "drizzle-orm";
 
 export default async function ArticlePage ({ params }: { params: { id: string } }) {
-    const { id } = await params
+    const { id } = params
     const [article] = await db.select().from(ArticleTable).where(eq(ArticleTable.id, id)).limit(1); // import
     return (
         <ArticleView article={article} />
